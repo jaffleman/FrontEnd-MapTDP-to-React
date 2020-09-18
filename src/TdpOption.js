fc7c9977610644685f1d78a450790e2ca718d028
jaffleman <jaffleman@gmail.com>
2020-09-18 07:46:50 +0200

    correction d'un petit bug sur affiche de TdpOption

--- src/TdpOption.js
+++ src/TdpOption.js
@@ -1,7 +1,8 @@
 import React from 'react'
 class TdpOption extends React.Component{
+   
     render(){
-        return(this.props.opt !== null? <div className = "optTNI">{this.props.data.opt}</div>:null)
+        return(this.props.opt !== null? <div className = "optTNI">{this.props.opt}</div>:null)
     }
 }
 export default TdpOption
