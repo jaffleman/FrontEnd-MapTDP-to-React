ab1bdf88b8e3b487e3665487d61c48ed32d37054
jaffleman <jaffleman@gmail.com>
2021-05-21 07:54:37 +0200

    Recherche de réglette sans position issue #20

--- src/classes/plot.js
+++ src/classes/plot.js
@@ -3,7 +3,8 @@ export class Plot {
     constructor(text) {
         const div = text.split(' ');
         this.rep = div[0].slice(0, 5).toLowerCase();
-        this.cd = parseInt(div[0].slice(3,5)) 
+        this.cd = parseInt(div[0].slice(3,5))
+        if (div[1].length<8) div[1] = div[1]+'xxx'
         this.regletteType = div[1].length === 9 ? (div[1].slice(0, 1) + '/' + div[1].slice(1, 4)).toUpperCase() : div[1].slice(0, 5).toUpperCase();
         this.regletteNbr =  div[1].slice(-5, -3).toUpperCase();
         this.plot = [div[1].slice(-3)]
