6604def59546f627ff547fea2439bbcfcb5d85d6
jaffleman <jaffleman@gmail.com>
2021-05-29 11:58:51 +0200

    finalise l'Intégration des réglettes dont la position n'a pas été trouvée

--- src/components/repCreator/RegletteConstructor.js
+++ src/components/repCreator/RegletteConstructor.js
@@ -4,44 +4,47 @@ import Button from 'react-bootstrap/Button'
 import { tdpExist } from '../../functions/tdpExist';
 
 class RegletteConstructor extends React.Component{
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            nd:this.props.nd,
-            val:this.props.val,
+            nd: this.props.nd,
+            val: this.props.val,
         }
     }
-    HeadHandleChange = (e) =>{
-        if (!tdpExist(this.props.session, e.target.value, this.props.val.tdps.regletteNbr)) {
-            const newSession = this.props.session.modifRegType([this.props.val.tdps._id], e.target.value)
+    HeadHandleChange = (e) => {
+        if (!tdpExist(this.props.session, e.target.value, this.props.val.tdp.regletteNbr)) {
+            const newSession = this.props.session.modifRegType([this.props.val.tdp._id], e.target.value)
             this.props.dispatch({
-                type:'SET_SESSION_DATA',
+                type: 'SET_SESSION_DATA',
                 value: newSession
             })
         }
     }
-    EndHandleChange = (e) =>{
-        const newSession = this.props.session.modifOption([this.props.val.tdps._id], e.target.value)
+    EndHandleChange = (e) => {
+        const newSession = this.props.session.modifOption([this.props.val.tdp._id], e.target.value)
         this.props.dispatch({
-            type:'SET_SESSION_DATA',
+            type: 'SET_SESSION_DATA',
             value: newSession
         })
     }
-    BodyHandleChange(e){
-        if (!tdpExist(this.props.session, this.props.val.tdps.regletteType, e.target.value)) {
-            const newSession = this.props.session.modifRegNbr([this.props.val.tdps._id], e.target.value)
+    BodyHandleChange(e) {
+        if (!tdpExist(this.props.session, this.props.val.tdp.regletteType, e.target.value)) {
+            const newSession = this.props.session.modifRegNbr([this.props.val.tdp._id], e.target.value)
             this.props.dispatch({
-                type:'SET_SESSION_DATA',
+                type: 'SET_SESSION_DATA',
                 value: newSession
             })
-            if (e.target.value.length===2) this.props.nextFocus(parseInt(this.props.nd))
+            if (e.target.value.length === 2) this.props.nextFocus(parseInt(this.props.nd))
         }
     }
-    deleteHandleClick(){
-        const newSession = this.props.session.modifRegType([this.props.val.tdps._id], 'x')
-        const newSession2 = newSession.modifRegNbr([this.props.val.tdps._id], '')
-        const newSession3 = newSession2.modifOption([this.props.val.tdps._id], '')
-        this.props.dispatch({type:'SET_SESSION_DATA',value: newSession3})
+    deleteHandleClick() {
+            const newSession = this.props.session.modifRegType([this.props.val.tdp._id], 'x')
+            const newSession2 = newSession.modifRegNbr([this.props.val.tdp._id], '')
+            const newSession3 = newSession2.modifOption([this.props.val.tdp._id], '')
+            this.props.dispatch({
+                type: 'SET_SESSION_DATA',
+                value: newSession3
+            })
     }
     render(){
         //console.log(this.props.session)
@@ -52,7 +55,7 @@ class RegletteConstructor extends React.Component{
                 </div>
                 
                 <div style={{marginLeft:'15px'}}>
-                    <select className="custom-select custom-select-sm" value={this.props.val.tdps.regletteType}  id={`select1:${this.props.val.tdps._id}${this.props.keyOrigin}`} style={{textAlign:"right"}} onChange={this.HeadHandleChange.bind(this)}>
+                    <select className="custom-select custom-select-sm" value={this.props.val.tdp.regletteType}  id={`select1:${this.props.val.tdp._id}${this.props.keyOrigin}`} style={{textAlign:"right"}} onChange={this.HeadHandleChange.bind(this)}>
                         <option value="x" defaultValue>-----</option>
                         <option value="L/INX">L/INX</option>
                         <option value="R/DEG">R/DEG</option>
@@ -62,19 +65,19 @@ class RegletteConstructor extends React.Component{
                 </div>
                 
                 <input 
-                    id={`input:${this.props.val.tdps._id}${this.props.keyOrigin}`}
+                    id={`input:${this.props.val.tdp._id}${this.props.keyOrigin}`}
                     ref= {this.props.laRef}
                     type="text" 
                     maxLength="2" 
                     style={{width:"46px"}} 
                     className="form-control form-control-sm"
-                    value={this.props.val.tdps.regletteNbr}
+                    value={this.props.val.tdp.regletteNbr}
                     onChange = {this.BodyHandleChange.bind(this)} 
                     onBlur={()=>{}}
                 />
                 
                 <div style={{marginLeft:'10px', width:'70px'}}>
-                    <select className="custom-select custom-select-sm" value={this.props.val.tdps.option===null?'...':this.props.val.tdps.option} id={`${this.props.val.tdps._id}option${this.props.keyOrigin}`} onChange={this.EndHandleChange.bind(this)} style={{}}>
+                    <select className="custom-select custom-select-sm" value={this.props.val.tdp.option===null?'...':this.props.val.tdp.option} id={`${this.props.val.tdp._id}option${this.props.keyOrigin}`} onChange={this.EndHandleChange.bind(this)} style={{}}>
                         <option value="null">...</option>
                         <option value="I">Inversée</option>
                         <option value="TNI">Tête Non Isolable</option>
--- src/functions/tdpExist.js
+++ src/functions/tdpExist.js
@@ -1,32 +1,40 @@
-export function tdpExist(session, regletteType, regletteNbr){
-    console.log(session)
+export function tdpExist(session, regletteType, regletteNbr) {
     if (regletteNbr === '' || regletteType === 'x') return false
     const tabTdp = []
-    session.rep.forEach(leRep => leRep.salle.forEach(laSalle => laSalle.rco.forEach(laRco => laRco.ferme.forEach(laFerme => laFerme.level.forEach(leLevel =>  tabTdp.push(leLevel.tdps))))))
-    
-    if (typeof(regletteNbr) === 'object') {
+    session.rep.forEach(leRep => leRep.salle.forEach(laSalle => laSalle.rco.forEach(laRco => laRco.ferme.forEach(laFerme => laFerme.level.forEach(leLevel => tabTdp.push(leLevel.tdp))))))
+    console.log(tabTdp)
+    if (typeof (regletteNbr) === 'object') {
         const tabNbr = []
         regletteNbr.forEach(element => {
-            const elemToChange = tabTdp.find(tdp=>tdp._id === element && (tdp.regletteType === 'x' || tdp.regletteType !== regletteType))
+            const elemToChange = tabTdp.find(tdp => tdp._id === element && (tdp.regletteType === 'x' || tdp.regletteType !== regletteType))
             if (elemToChange) tabNbr.push(elemToChange)
         })
         const existTab = []
         tabNbr.forEach(nbr => {
-            if (nbr.regletteNbr !== ''){
+            if (nbr.regletteNbr !== '') {
                 const exist = tabTdp.find(element => element.regletteType === regletteType && element.regletteNbr === nbr.regletteNbr)
                 if (exist) return existTab.push(exist)
-            } 
-        }) 
+            }
+        })
         if (existTab.length) {
-            alert("La ou les reglettes suivantes existent déjà dans ce répartiteur: \n" + existTab.map(elem => " "+regletteType + elem.regletteNbr))
+            alert("La ou les reglettes suivantes existent déjà dans ce répartiteur: \n" + existTab.map(elem => " " + regletteType + elem.regletteNbr))
             return true
-        }else return false
-    }else{
-        if (regletteNbr.length < 2  ) return false
+        } else return false
+    } else {
+        if (regletteNbr.length < 2) return false
         const exist = tabTdp.find(element => element.regletteType === regletteType && element.regletteNbr === regletteNbr)
         if (exist) {
-            alert("La reglette "+exist.regletteType+exist.regletteNbr+" existe déjà dans ce répartiteur!")
+            alert("La reglette " + exist.regletteType + exist.regletteNbr + " existe déjà dans ce répartiteur!")
             return true
         } else return false
     }
 }
+
+
+/*
+const session = JSON.parse('{"repName":"rip00","brutdata":[{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":1,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001111","position":11011},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":2,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001112","position":11012},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":3,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001113","position":11013},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":4,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001114","position":11014},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":5,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001115","position":11015},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":6,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001116","position":11016},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":7,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001117","position":11017},{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":8,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001118","position":11018}],"rep":[{"name":"rip00","salle":[{"number":1,"rco":[{"number":1,"ferme":[{"number":1,"index":0,"level":[{"number":1,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":1,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001111","position":11011}},{"number":2,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":2,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001112","position":11012}},{"number":3,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":3,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001113","position":11013}},{"number":4,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":4,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001114","position":11014}},{"number":5,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":5,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001115","position":11015}},{"number":6,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":6,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001116","position":11016}},{"number":7,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":7,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001117","position":11017}},{"number":8,"tdp":{"tdpId":"rip00x","status":"ghost","cd":"00","rep":"rip00","ferme":1,"level":8,"option":null,"rco":1,"regletteNbr":"","regletteType":"x","salle":1,"_id":"rip001118","position":11018}}]}]}]}]}],"cd":"00","salleNumber":1}' ) 
+const regletteType = 'L/INX'  
+//const regletteNbr =['rip001111','rip001112','rip001113','rip001114','rip001115','rip001116','rip001117','rip001118']
+const regletteNbr = 'G0'
+const result = tdpExist(session, regletteType, regletteNbr)//?
+*/
